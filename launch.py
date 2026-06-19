a49773f58cd52347f91210c0051ee86ecdccdee7
Joaquin Philco <joaquinphilco@hotmail.com>
2026-06-19 02:15:54 -0400

    webapp: Innit server and forward it on a public port.
    Signed-off-by: Joaquin Philco <joaquinphilco@hotmail.com>

--- launch.py
+++ launch.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
-# Import necessary modules like this:
-# from daemon.daemon import ConstellationDaemon
-# from webapp.app import create_app
+import os
+
+from pathlib import Path
 from camera.CameraManager import CameraManager
+from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+
+HOST = "0.0.0.0"
+PORT = 8000
+WEBAPP_DIR = Path(__file__).parent / "webapp"
 
 def main():
+    # Start the camera manager
     camera_manager = CameraManager()
+
+    # Start the web server to serve the control panel
+    os.chdir(WEBAPP_DIR)
+    server = ThreadingHTTPServer((HOST, PORT), SimpleHTTPRequestHandler)
+    print(f"Serving webapp on http://{HOST}:{PORT}/control_panel.html")
+    server.serve_forever()
+
 
 if __name__ == "__main__":
     main()
